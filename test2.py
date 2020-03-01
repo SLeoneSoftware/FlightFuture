e3e46804713cdba4fa147e7d0147d68d89188e4d
Steven Leone <SALeone1999@yahoo.com>
2020-03-02 10:01:25 +1100

    Added comments describing data

--- test2.py
+++ test2.py
@@ -1,4 +1,4 @@
-#This will move all data into a sqlite3 database, making it much easier to query
+
 
 import numpy
 import os
@@ -15,3 +15,39 @@ def get_data(file='flights.csv',path='flight-delays',seed=1067748903,datatype=st
 
 x, y = get_data()
 print(x)
+
+
+#Data By Column
+'''
+1 Year
+2 Month
+3 Day
+4 Day of Week
+5 Airline
+6 Flight Number
+7 Tail Number
+8 Origin Airport
+9 Destination Airport
+10 Scheduled Departure
+11 Departure Time
+12 Departure Delay
+13 Taxi Out
+14 Wheels Off
+15 Planned Trip Time
+16 AIR_TIME+TAXI_IN+TAXI_OUT
+17 The time duration between wheels_off and wheels_on time (air time)
+18 Distance between two airports
+19 Wheels On
+20 Taxi in
+21 Scheduled Arrival
+22 Arrival Time
+23 Arrival Delay
+24 Diverted
+25 Cancelled
+26 Cancellation Reason
+27 Air System Delay
+28 Security Delay
+29 Airline Delay
+30 Late Aircraft Delay
+31 Weather Delay
+'''
